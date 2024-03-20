05275d456356896d506d8b00b63a70f9c8d22424
na1307 <na1307@outlook.kr>
2024-03-20 14:52:59 +0900

    수정 -> 변경

--- SimpleSixtarScorecard/ProfileNameDialog.cs
+++ SimpleSixtarScorecard/ProfileNameDialog.cs
@@ -3,7 +3,7 @@
 public partial class ProfileNameDialog : Dialog {
     public ProfileNameDialog(bool createNew) {
         InitializeComponent();
-        label1.Text = createNew ? "이름이 무엇인가요?\r\n\r\n언제든지 수정할 수 있습니다." : "이름이 무엇인가요?";
+        label1.Text = createNew ? "이름이 무엇인가요?\r\n\r\n언제든지 변경할 수 있습니다." : "이름이 무엇인가요?";
     }
 
     public string UserName => textBox1.Text;
