264e290a6a45a9036e348aa09646edfbf506e1ac
na1307 <na1307@outlook.kr>
2024-03-22 15:51:22 +0900

    Update NuGet

--- SimpleSixtarScorecard/ProfileNameDialog.cs
+++ SimpleSixtarScorecard/ProfileNameDialog.cs
@@ -1,6 +1,6 @@
 ﻿namespace SimpleSixtarScorecard;
 
-public partial class ProfileNameDialog : Dialog {
+public partial class ProfileNameDialog : LocalizableDialog {
     public ProfileNameDialog(bool createNew) {
         InitializeComponent();
         label1.Text = createNew ? "What's your name?\r\n\r\nYou can always change it later." : "What's your name?";
