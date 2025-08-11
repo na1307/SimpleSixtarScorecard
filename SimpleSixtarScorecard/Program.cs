6407d1ab8ac6ec8885d82ba6f97bfc9a98cd7e70
Jonghyo Lee <na1307@outlook.kr>
2025-08-11 15:01:31 +0900

    Update Package 2

--- SimpleSixtarScorecard/Program.cs
+++ SimpleSixtarScorecard/Program.cs
@@ -10,54 +10,56 @@ internal static class Program {
     /// </summary>
     [STAThread]
     private static async Task Main() {
-        // To customize application configuration such as set high DPI settings or default font,
-        // see https://aka.ms/applicationconfiguration.
-        Application.EnableVisualStyles();
-        Application.SetCompatibleTextRenderingDefault(false);
+        try {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
 
-        // Verify songdata.json
-#if NETCOREAPP3_0_OR_GREATER
-        await
-#endif
-        using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SimpleSixtarScorecard.songdata.schema.json")) {
-            if (stream == null) {
-                ErrMsg("Song data schema not found!");
-                return;
-            }
-
+            // Verify songdata.json
 #if NETCOREAPP3_0_OR_GREATER
             await
 #endif
-            using FileStream songdata = new("songdata.json", FileMode.Open, FileAccess.Read);
-            using var json = await JsonDocument.ParseAsync(songdata);
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SimpleSixtarScorecard.songdata.schema.json")) {
+                if (stream == null) {
+                    ErrMsg("Song data schema not found!");
+                    return;
+                }
 
-            if (!(await JsonSchema.FromStream(stream)).Evaluate(json).IsValid) {
-                ErrMsg("Song data is invalid!");
-                return;
-            }
-        }
-
-        // Profile creation
-        if (!File.Exists(Profile.ProfileFile)) {
-            using ProfileNameDialog dialog = new(true);
-
-            if (dialog.ShowDialog() == DialogResult.OK) {
 #if NETCOREAPP3_0_OR_GREATER
                 await
 #endif
-                using FileStream fs = new(Profile.ProfileFile, FileMode.CreateNew, FileAccess.Write);
-                await using Utf8JsonWriter writer = new(fs);
+                using FileStream songdata = new("songdata.json", FileMode.Open, FileAccess.Read);
+                using var json = await JsonDocument.ParseAsync(songdata);
 
-                writer.WriteStartObject();
-                writer.WriteString(Profile.UserNamePropertyName, dialog.UserName.Trim());
-                writer.WriteStartArray(Profile.ResultsPropertyName);
-                writer.WriteEndArray();
-                writer.WriteEndObject();
-            } else {
-                return;
+                if (!(await JsonSchema.FromStream(stream)).Evaluate(json).IsValid) {
+                    ErrMsg("Song data is invalid!");
+                    return;
+                }
             }
-        }
 
-        Application.Run(new MainForm());
+            // Profile creation
+            if (!File.Exists(Profile.ProfileFile)) {
+                using ProfileNameDialog dialog = new(true);
+
+                if (dialog.ShowDialog() == DialogResult.OK) {
+#if NETCOREAPP3_0_OR_GREATER
+                    await
+#endif
+                    using FileStream fs = new(Profile.ProfileFile, FileMode.CreateNew, FileAccess.Write);
+                    await using Utf8JsonWriter writer = new(fs);
+
+                    writer.WriteStartObject();
+                    writer.WriteString(Profile.UserNamePropertyName, dialog.UserName.Trim());
+                    writer.WriteStartArray(Profile.ResultsPropertyName);
+                    writer.WriteEndArray();
+                    writer.WriteEndObject();
+                } else {
+                    return;
+                }
+            }
+
+            Application.Run(new MainForm());
+        } catch (Exception e) {
+            ErrMsg(e.ToString());
+        }
     }
 }
