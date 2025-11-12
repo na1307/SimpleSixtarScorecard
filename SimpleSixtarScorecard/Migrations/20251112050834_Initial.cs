1436c4336e897e73683ec8be8bd7ae4d691fec4e
Jonghyo Lee <na1307@outlook.kr>
2025-11-12 14:16:31 +0900

    Migrations

---
+++ SimpleSixtarScorecard/Migrations/20251112050834_Initial.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleSixtarScorecard.Migrations;
+
+/// <inheritdoc />
+public sealed partial class Initial : Migration {
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder) => migrationBuilder.CreateTable(
+            name: "Results",
+            columns: table => new {
+                SongId = table.Column<string>(type: "TEXT", nullable: false),
+                Mode = table.Column<string>(type: "TEXT", nullable: false),
+                Difficulty = table.Column<string>(type: "TEXT", nullable: false),
+                Score = table.Column<int>(type: "INTEGER", nullable: false),
+                FullCombo = table.Column<bool>(type: "INTEGER", nullable: false)
+            },
+            constraints: table => {
+                table.PrimaryKey("PK_Results", x => new { x.SongId, x.Mode, x.Difficulty });
+            });
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder) => migrationBuilder.DropTable(name: "Results");
+}
